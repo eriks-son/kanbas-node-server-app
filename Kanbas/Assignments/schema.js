f9e7a2286bae530efbf6b219e6bec962cb0f7f6d
August Eriksson <antgus03@gmail.com>
2024-12-05 17:19:26 -0500

    Fix assignments

--- Kanbas/Assignments/schema.js
+++ Kanbas/Assignments/schema.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 const schema = new mongoose.Schema(
   {
     title: String,
-    description: String,
     course: { type: mongoose.Schema.Types.ObjectId, ref: "CourseModel" },
   },
   { collection: "assignments" }
